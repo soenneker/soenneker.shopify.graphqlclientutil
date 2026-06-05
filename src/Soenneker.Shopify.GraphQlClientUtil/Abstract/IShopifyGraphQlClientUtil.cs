1ac7b1a7e95edc9f38360adc6733bb2bf189a0e8
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:45 -0500

    xml commenting

--- src/Soenneker.Shopify.GraphQlClientUtil/Abstract/IShopifyGraphQlClientUtil.cs
+++ src/Soenneker.Shopify.GraphQlClientUtil/Abstract/IShopifyGraphQlClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Shopify.GraphQlClientUtil.Abstract;
 /// </summary>
 public interface IShopifyGraphQlClientUtil : IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<ShopifyGraphQlClient> Get(CancellationToken cancellationToken = default);
 }
